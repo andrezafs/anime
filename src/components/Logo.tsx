e252fe9223f86956a4d2632775b8721896e572d1
Andreza <andrezafs.rr@gmail.com>
2023-01-24 18:31:20 -0300

    feat: create logo component

---
+++ src/components/Logo.tsx
@@ -0,0 +1,12 @@
+import { styled } from "../styles";
+
+const Container = styled("a", {
+  fontFamily: "$rubik",
+  fontWeight: "$medium",
+  color: "$primary",
+  fontSize: "$4xl",
+});
+
+export function Logo() {
+  return <Container href="#">Ani.me</Container>;
+}
