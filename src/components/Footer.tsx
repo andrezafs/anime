7c36b1e7a87a449a71744aca2fa865c1d3829daf
Andreza <andrezafs.rr@gmail.com>
2023-01-31 19:57:55 -0300

    feat: create component footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,25 @@
+import { styled } from "../styles";
+
+const Container = styled("footer", {
+  textAlign: "center",
+  padding: "$12 0",
+});
+
+const Description = styled("span", {
+  fontFamily: "$inter",
+  fontSize: "$lg",
+  fontWeight: "$normal",
+  color: "$quinary",
+});
+
+type footerProps = {
+  description: string;
+};
+
+export function Footer({ description, ...props }: footerProps) {
+  return (
+    <Container {...props}>
+      <Description>{description}</Description>
+    </Container>
+  );
+}
